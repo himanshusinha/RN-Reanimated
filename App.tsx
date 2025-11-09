6ea1f32a4a307b0fe85228effe040b1aed96de23
hsinha1110 <97324633+hsinha1110@users.noreply.github.com>
2025-11-09 08:22:48 +0530

    spring

--- App.tsx
+++ App.tsx
@@ -1,37 +1,45 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withDelay,
+  withSpring,
+  withTiming,
+} from 'react-native-reanimated';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
+export default function App() {
+  const animatedValue = useSharedValue(1); // start visible
+  const animatedHeight = useSharedValue(100);
+  const animatedWidth = useSharedValue(100);
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: animatedValue.value,
+      height: animatedHeight.value,
+      width: animatedWidth.value,
+    };
+  });
+  console.log('re render value');
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <Animated.View style={[styles.box, animatedStyle]} />
+
+      <TouchableOpacity
+        onPress={() => {
+          if (animatedValue.value === 1) {
+            animatedValue.value = withTiming(0.5, { duration: 1000 });
+            animatedHeight.value = withSpring(100);
+            animatedWidth.value = withSpring(100);
+          } else {
+            animatedValue.value = withTiming(1, { duration: 1000 });
+            animatedHeight.value = withSpring(50);
+            animatedWidth.value = withSpring(50);
+          }
+        }}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Change Opacity</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -39,7 +47,22 @@ function AppContent() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+    marginBottom: 20,
+  },
+  button: {
+    paddingHorizontal: 16,
+    paddingVertical: 10,
+    backgroundColor: '#eee',
+    borderRadius: 8,
+  },
+  buttonText: {
+    fontSize: 16,
   },
 });
-
-export default App;
